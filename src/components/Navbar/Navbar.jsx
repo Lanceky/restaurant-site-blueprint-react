e7b913a90ad0fcefa631404fb3389fb848ad825b
Predator <evanslance100@gmail.com>
2024-12-24 14:15:54 +0300

    added list

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,6 +25,14 @@ const Navbar = () => (
     </div>
     <div className='app__navbar-smallscreen'>
       <GiHamburgerMenu color='#fff' fontSize={27} onClick={()=>{}}/>
+      <div className='app__navbar-smallscreen_overlay flex__center-slide-bottom'></div>
+      <ul className='app__navbar-smallscreen-links'>
+        <li className='p__opensans'><a href="#home">Home</a></li>
+        <li className='p__opensans'><a href="#about">About</a></li>
+        <li className='p__opensans'><a href="#menu">Menu</a></li>
+        <li className='p__opensans'><a href="#awards">Awards</a></li>
+        <li className='p__opensans'><a href="#contact">Contact</a></li>
+      </ul>
     </div>
   </nav>
 );
